e785a7274df4c8fbdb4a4bca8f0a8e22e50df0c3
Garbolino <orlanmarti@gmail.com>
2020-10-03 12:45:37 +0200

    define genesis block

--- src/validation.cpp
+++ src/validation.cpp
@@ -141,7 +141,7 @@ bool fRequireStandard = true;
 bool fCheckBlockIndex = false;
 bool fCheckpointsEnabled = DEFAULT_CHECKPOINTS_ENABLED;
 uint64_t nPruneTarget = 0;
-int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
+int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE * 4380; // 12 years
 
 uint256 hashAssumeValid;
 arith_uint256 nMinimumChainWork;
